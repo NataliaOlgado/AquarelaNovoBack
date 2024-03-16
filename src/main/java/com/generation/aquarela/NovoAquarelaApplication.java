d624c6a045aadc6fc3134963d4637a73a7dd4a4e
NataliaOlgado <natalia.olgado@hotmail.com>
2024-03-16 01:37:40 -0300

    main

---
+++ src/main/java/com/generation/aquarela/NovoAquarelaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.aquarela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoAquarelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoAquarelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/aquarela/NovoAquarelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.aquarela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoAquarelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
